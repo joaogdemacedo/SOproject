4fefe698ac15f838e7066b15a380fd863f43faea
joaogdemacedo <joaogdemacedo@gmail.com>
2022-09-07 11:25:10 +0100

    Add readme

---
+++ rust/target/debug/deps/rust-bf3c0b6c5d47c68a.d
@@ -0,0 +1,5 @@
+/home/diguest/Desktop/SOproject/rust/target/debug/deps/rust-bf3c0b6c5d47c68a: src/main.rs
+
+/home/diguest/Desktop/SOproject/rust/target/debug/deps/rust-bf3c0b6c5d47c68a.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/diguest/Desktop/SOproject/rust/target/debug/rust: /home/diguest/Desktop/SOproject/rust/src/main.rs
